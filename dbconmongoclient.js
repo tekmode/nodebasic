b57719ab8da10e6b651402722b0602ab7c2d4ba7
Muhammad Rashid Mukhtar <38366545+tekmode@users.noreply.github.com>
2021-03-17 16:23:01 +0500

    Update dbconmongoclient.js

--- dbconmongoclient.js
+++ dbconmongoclient.js
@@ -1,6 +1,5 @@
 var MongoClient = require('mongodb').MongoClient;
 var url1 = "mongodb://localhost:27017";
-var url = "mongodb://localhost:27017/assessmentportal";
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
@@ -27,16 +26,16 @@ res.end(`Hello <strong>home page</strong><br>
 } 
 else if (req.url === '/addemp' && req.method === 'GET') {
     
-    MongoClient.connect(url1, { useNewUrlParser: true }, function(err, db) {
+     MongoClient.connect(url1, { useNewUrlParser: true,useUnifiedTopology: true }, function(err, db) {
           if (err) throw err;
-          var dbo = db.db("assessmentportal");
-         dbo.collection("reviews").group(
-            {
-              key: { 'course.courseTeacher':1,'course.courseName':1 },
-              reduce: function ( curr, result ) { result.count++;},
-              initial: {count:0 }
-            }
-         );
+         console.log("Database created!");
+         var dbo = db.db("mydb");
+         var myobj = { name: "Human", address: "Pakistan" };
+         dbo.collection("customers").insertOne(myobj, function(err, result) {
+          if (err) throw err;
+            console.log("1 document inserted");
+         res.end('Record Added Successfully');
+           	});        
 });
       
 } else if (req.url === '/upemp' && req.method === 'GET') {
